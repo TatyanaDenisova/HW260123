bad51f795e0d19f7091e6e6ed81f72349a17fd09
Tatiana Denisova <2746619@gmail.com>
2023-02-03 23:39:26 +0300

    Add Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,75 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по
+ убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/ 
+int[,] generate2DArray(int rowLength, int colLength, int start, int finish)
+{
+    int[,] array = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+    }
+    return array;
+}
+void printInColour(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void printHaedOfArray(int length)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColour(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+void printArray(int[,] array)
+{
+    printHaedOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColour(i + "|\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    System.Console.WriteLine("-----------------------------------------------");
+}
+void ranjirRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           for (int y = 0; y < array.GetLength(1)-1; y++)
+           {
+            if(array[i,y]<array[i,y+1])
+            {
+                int temp = 0;
+                temp = array[i,y];
+                array[i,y]= array[i,y+1];
+                array[i, y+1] = temp;
+            }
+           } 
+        }
+    }
+}
+int[,] array = generate2DArray(5, 6, 0, 10);
+printArray(array);
+ranjirRow(array);
+printArray(array);
